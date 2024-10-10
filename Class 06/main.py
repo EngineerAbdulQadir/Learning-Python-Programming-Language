a472e9046d1f4d6d161a3222185e0d9c3bb3ba89
EnggAbdulQadir <abdulqadirimran001@gmail.com>
2024-10-10 17:25:03 +0500

    Update

---
+++ Class 06/main.py
@@ -0,0 +1,21 @@
+a = complex(8, 2)
+b = True
+c = "Abdul Qadir"
+d = None
+print(a)
+print(b)
+a1 = 9
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dict1 = {"name":"Rida", "age":16, "canVote":True}
+print(dict1)
